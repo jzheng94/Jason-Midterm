347c9923f92ad34fd3b32fe8a9b21fc9a02d7f21
J.Zheng <43321744+jzheng94@users.noreply.github.com>
2022-05-30 17:44:22 -0400

    Permutation

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -1,10 +1,23 @@
 package string.problems;
-
+import java.util.Scanner;
 /**
  * Created by mrahman on 04/22/17.
  */
 public class Permutation {
 
+    static void printPermutation(String string, String perm){
+        if (string.length() == 0){
+            System.out.println(perm + " ");
+            return;
+        }
+
+        for ( int a = 0; a < string.length(); a++){
+            char character = string.charAt(a);
+            String remainder = string.substring(0, a) + string.substring(a + 1);
+
+            printPermutation(remainder, perm + character);
+        }
+    }
     public static void main(String[] args) {
 
         /*
@@ -12,5 +25,9 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please input a string: ");
+        String string2 = input.nextLine();
+        printPermutation(string2, "");
     }
 }
