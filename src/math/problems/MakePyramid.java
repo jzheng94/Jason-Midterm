c2c8a222eed36d41b5de47696dabb5252b3429fa
J.Zheng <43321744+jzheng94@users.noreply.github.com>
2022-06-03 23:16:08 -0400

    Pyramid

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,4 +1,5 @@
 package math.problems;
+import java.io.*;
 
 public class MakePyramid {
     public static void main(String[] args) {
@@ -13,7 +14,14 @@ public class MakePyramid {
                          * * * * * *
 
         */
-
-
+        for (int i = 0; i < 6; i++) {
+            for (int h = 0; h < 10 - i; h++) {
+                System.out.print(" ");
+            }
+            for (int l = 0; l <= i; l++) {
+                System.out.print(" *");
+            }
+            System.out.println();
         }
-  }
+    }
+}
