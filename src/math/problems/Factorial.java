24717fba91a767f3117719f22c0382cf28d8374c
J.Zheng <43321744+jzheng94@users.noreply.github.com>
2022-06-03 22:36:37 -0400

    factorial

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -11,6 +11,29 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        int num = 10;
+        int factorialRecursionAnswer = factorialRecursion(num);
+        int factorialIterateAnswer = factorialIteration(num);
+        System.out.println("The factorial of " + num + " via recursion is " + factorialRecursionAnswer);
+        System.out.println("The factorial of " + num + " via iteration is " + factorialIterateAnswer);
+    }
 
+    private static int factorialIteration(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        }
+        int answer = 1;
+        for (int i = 2; i <= num; i ++) {
+            answer *= i;
+        }
+        return answer;
+    }
+
+    private static int factorialRecursion(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        }
+        return num * factorialRecursion(num-1);
     }
 }
+
