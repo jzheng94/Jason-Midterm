d891c23376b198db75b1cae0ee8dd29c7f50250c
J.Zheng <43321744+jzheng94@users.noreply.github.com>
2022-06-03 22:41:56 -0400

    Fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,7 +6,17 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
+        int num1 = 0, num2 = 1;
+        int counter = 0;
 
+        while(counter < 40){
+            System.out.println(num1 + " ");
+
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            counter = counter + 1;
+        }
 
     }
 }
